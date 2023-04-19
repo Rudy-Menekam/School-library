8eafed37293692a9d1156f573b3327fb7fbfd80e
Menekam Rudy <menekamrudy@gmail.com>
2023-04-19 13:17:40 +0100

    Fix lint error

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,6 +2,7 @@
 require_relative 'decorator'
 class CapitalizeDecorator < Decorator
   attr_accessor :nameable
+
   def correct_name
     nameable.correct_name.capitalize
   end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,7 @@
 require_relative 'decorator'
 class TrimmerDecorator < Decorator
   attr_accessor :nameable
+
   def correct_name
     name = nameable.correct_name
     name.length > 10 ? name[0, 10] : name
