154dccb527322f526743ca1892526832934f8372
Menekam Rudy <menekamrudy@gmail.com>
2023-04-19 13:16:43 +0100

    Fix local variable error

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,6 +1,7 @@
 # Capitalize Decorator Class
 require_relative 'decorator'
 class CapitalizeDecorator < Decorator
+  attr_accessor :nameable
   def correct_name
     nameable.correct_name.capitalize
   end
