0362b3db23298c3f14aea371e6df4e8228f8cf17
Menekam Rudy <menekamrudy@gmail.com>
2023-04-20 20:33:54 +0100

    add basic ui

---
+++ main.rb
@@ -0,0 +1,17 @@
+require_relative 'app'
+
+def main
+  puts "Welcome to School library App!\n"
+  app = App.new
+  loop do
+    app.display_selection
+    option = gets.chomp.to_i
+    case option
+    when 1..6
+      app.list_holder(option)
+    else
+      puts 'Thank you for using this app!'
+      break
+    end
+  end
+end
